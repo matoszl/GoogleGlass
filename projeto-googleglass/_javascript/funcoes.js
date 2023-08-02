f93b42324173d1705554230829e30e90e4aed89f
Pedro Matos <117551662+matoszl@users.noreply.github.com>
2023-08-02 20:21:48 -0300

    Add files via upload

---
+++ projeto-googleglass/_javascript/funcoes.js
@@ -0,0 +1,6 @@
+function mudarFoto(foto){
+    document.getElementById("icone").src = foto;
+}
+function padraofoto(){
+    document.getElementById("icone").src= "_imagens/glass-oculos-preto-peq.png";
+}
